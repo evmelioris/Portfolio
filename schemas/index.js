2d5f2514e5b7762bbf019aba4e364f8424aed61d
Evan Melioris <epmelioris@gmail.com>
2022-12-20 14:42:46 -0500

    Initial Deployment

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,10 @@
-export const schemaTypes = []
+import testimonials from './testimonials';
+import works from './works';
+import brands from './brands';
+import contact from './contact';
+import experiences from './experiences';
+import abouts from './abouts';
+import skills from './skills';
+import workExperience from './workExperience';
+
+export const schemaTypes = [works, testimonials, brands, abouts, skills, workExperience, experiences, contact];
